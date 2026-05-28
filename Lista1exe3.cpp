9da08568393901da16f687244536d606afbdee15
Ildinei <ildinei.r@aluno.ifsp.edu.br>
2026-05-28 20:15:44 -0300

    Add files via upload

---
+++ Lista1exe3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int diagonal;
+    
+    cout << "Digite o valor da diagonal do quadrado: ";
+    cin >> diagonal;
+    cout << "A area do quadrado e: " << (diagonal * diagonal) / 2.0 << endl;
+    
+    return 0;
+}
