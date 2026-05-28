2dc0154ad66fccdbdc16faa339502df02865e90a
Ildinei <ildinei.r@aluno.ifsp.edu.br>
2026-05-28 20:15:17 -0300

    Add files via upload

---
+++ Lista1exe2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int aresta;
+    
+    cout << "Digite o valor da aresta do quadrado: ";
+    cin >> aresta;
+    cout << "A area do quadrado e: " << aresta * aresta << endl;
+    
+    return 0;
+}
